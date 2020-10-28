647ccf0c0154c0e937276563595415cd4f96e615
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-29 00:18:37 +0530

    Modified: Calculation of Employee's Parttime Daily wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,24 @@
-public class EmployeeWageComputation
-{
-	public static void main(String args[])
-	{
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	}
+import java.util.Random;
+
+
+public class EmployeeWageComputation {
+      public static void main(String args[])
+      {
+		  final int Fulltime=8;
+		  final int Parttime=8;
+		  final int EmployeeRatePerHour=20;
+	          System.out.println("Welcome to Employee Wage Computation Program");
+                  Random rand = new Random();
+                  int attendance=rand.nextInt(2);
+			  if(attendance==0)
+			  {
+				 int workdone=rand.nextInt(2);
+				 if(workdone==0)
+					System.out.println("Emplyoyee's Daily Wage "+Fulltime*EmployeeRatePerHour);
+				 else	
+					System.out.println("Emplyoyee's Daily Wage "+Parttime*EmployeeRatePerHour);
+			  }
+                  	else
+                  		System.out.println("Emplyoyee is Absent");
+      }
 }
