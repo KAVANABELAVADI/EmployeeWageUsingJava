8c2a58e4f190b4f6e6f15df16d1f8ba0108273b9
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-29 01:14:09 +0530

    Modified: Calculating Wages Per Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,24 +1,54 @@
 import java.util.Random;
 
-
+class Computation{
+	final int Fulltime=8;
+	final int Parttime=8;
+	final int EmployeeRatePerHour=20;
+	int WorkDonePerDay,dailyWage,totalWage,hours_per_month=0,days_per_month=0 ;
+	int days=20;
+	Random rand = new Random();
+	void calculationOfTotalWage()
+	{
+		while(days_per_month!=days)
+		{
+		int attendance=rand.nextInt(2);
+		if(attendance==0)
+		{
+			days_per_month++;
+		    WorkDonePerDay=getWorkDonePerDay();
+			hours_per_month=hours_per_month+WorkDonePerDay;
+			dailyWage=WorkDonePerDay*EmployeeRatePerHour;
+			totalWage=dailyWage+totalWage;
+		}
+		else
+			days--;
+		}	
+	}
+	void print()
+	{
+		System.out.println("Working Days of Employee :"+days_per_month);
+		System.out.println("DailyWage of Employee :"+dailyWage);
+		System.out.println("TotalWage of Employee :"+totalWage);
+	}
+	int getWorkDonePerDay()
+	{
+		int workdone=rand.nextInt(2);
+		switch (workdone) {
+			case 0: WorkDonePerDay=Fulltime;
+					break;
+			case 1: WorkDonePerDay=Parttime;
+					break;
+			default: WorkDonePerDay=0;
+		}
+		return WorkDonePerDay;
+	}
+}
 public class EmployeeWageComputation {
       public static void main(String args[])
       {
-		  final int Fulltime=8;
-		  final int Parttime=8;
-		  final int EmployeeRatePerHour=20;
-	          System.out.println("Welcome to Employee Wage Computation Program");
-                  Random rand = new Random();
-                  int attendance=rand.nextInt(2);
-			  if(attendance==0)
-			  {
-				 int workdone=rand.nextInt(2);
-				 if(workdone==0)
-					System.out.println("Emplyoyee's Daily Wage "+Fulltime*EmployeeRatePerHour);
-				 else	
-					System.out.println("Emplyoyee's Daily Wage "+Parttime*EmployeeRatePerHour);
-			  }
-                  	else
-                  		System.out.println("Emplyoyee is Absent");
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Computation calculation=new Computation();
+		calculation.calculationOfTotalWage();    
+		calculation.print();      
       }
 }
