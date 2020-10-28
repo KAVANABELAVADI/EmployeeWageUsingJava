97fe873f612935398b95cf57d37a16551ff150d7
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-29 00:54:08 +0530

    Modified: Using Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,24 +1,48 @@
 import java.util.Random;
 
-
+class Computation{
+	final int Fulltime=8;
+	final int Parttime=8;
+	final int EmployeeRatePerHour=20;
+	int WorkDonePerDay,dailyWage,totalWage,hours_per_month=0 ;
+	Random rand = new Random();
+	void calculationOfTotalWage()
+	{
+		int attendance=rand.nextInt(2);
+		if(attendance==0)
+		{
+		    WorkDonePerDay=getWorkDonePerDay();
+			hours_per_month=hours_per_month+WorkDonePerDay;
+			dailyWage=WorkDonePerDay*EmployeeRatePerHour;
+			
+		}
+		else
+			System.out.println("Emplyoyee is Absent");
+	}
+	void print()
+	{
+		System.out.println("DailyWage of Employee :"+dailyWage);
+		
+	}
+	int getWorkDonePerDay()
+	{
+		int workdone=rand.nextInt(2);
+		switch (workdone) {
+			case 0: WorkDonePerDay=Fulltime;
+					break;
+			case 1: WorkDonePerDay=Parttime;
+					break;
+			default: WorkDonePerDay=0;
+		}
+		return WorkDonePerDay;
+	}
+}
 public class EmployeeWageComputation {
       public static void main(String args[])
       {
-		  final int Fulltime=8;
-		  final int Parttime=8;
-		  final int EmployeeRatePerHour=20;
-	          System.out.println("Welcome to Employee Wage Computation Program");
-                  Random rand = new Random();
-                  int attendance=rand.nextInt(2);
-			  if(attendance==0)
-			  {
-				 int workdone=rand.nextInt(2);
-				 if(workdone==0)
-					System.out.println("Emplyoyee's Daily Wage "+Fulltime*EmployeeRatePerHour);
-				 else	
-					System.out.println("Emplyoyee's Daily Wage "+Parttime*EmployeeRatePerHour);
-			  }
-                  	else
-                  		System.out.println("Emplyoyee is Absent");
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Computation calculation=new Computation();
+		calculation.calculationOfTotalWage();    
+		calculation.print();      
       }
 }
