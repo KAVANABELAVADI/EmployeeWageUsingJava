89dc4d8e7ffda5aa1b48e17e53d3758c0ace42f6
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-28 23:25:30 +0530

    Modified: Checking for attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,14 @@
-public class EmployeeWageComputation
-{
-	public static void main(String args[])
-	{
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	}
+import java.util.Random;
+
+public class EmployeeWageComputation {
+      public static void main(String args[])
+      {
+    	System.out.println("Welcome to Employee Wage Computation Program");
+                  Random rand = new Random();
+                  int attendance=rand.nextInt(2);
+                  if(attendance==0)
+                  System.out.println("Emplyoyee is Present");
+                  else
+                  System.out.println("Emplyoyee is Absent");
+      }
 }
