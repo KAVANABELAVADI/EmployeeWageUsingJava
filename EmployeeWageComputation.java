51e3fb00d28e2476c4550acd9e80ddae2931c28f
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-29 00:12:05 +0530

    Modified: Calculation of Employee's Fulltime Daily wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 
+
 public class EmployeeWageComputation {
       public static void main(String args[])
       {
@@ -8,9 +9,9 @@ public class EmployeeWageComputation {
 	          System.out.println("Welcome to Employee Wage Computation Program");
                   Random rand = new Random();
                   int attendance=rand.nextInt(2);
-                  if(attendance==0)
-                  System.out.println("Emplyoyee's Daily Wage "+Fulltime*EmployeeRatePerHour);
-                  else
-                  System.out.println("Emplyoyee is Absent");
+		  	if(attendance==0)
+				System.out.println("Emplyoyee's Daily Wage "+Fulltime*EmployeeRatePerHour);
+	                  else
+               			System.out.println("Emplyoyee is Absent");
       }
 }
